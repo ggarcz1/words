df65053e0677fbdb3ca22c80054e3596f225364f
ggarcz1 <54447564+ggarcz1@users.noreply.github.com>
2023-11-19 18:52:22 -0600

    initial testing with the API and a few words
    need to work on the spacing/parsing for the word:def pairs

--- wordsDatabase.py
+++ wordsDatabase.py
@@ -1,21 +1,24 @@
 import requests
+import time
+import json
 
 # open the file of words
-wordsFile = open('words.txt')
+wordsFile = open('words.txt','r')
+words_definition = open('output.txt', 'w')
+api_key = '186c73a1-3a44-4091-9e6d-a2cdf0d47608'
 
-# for word in wordsFile:
-#   response = requests.get('https://www.dictionary.com/browse/'+word)
-
-
-# https://dictionaryapi.com/products/api-collegiate-dictionary
-
-word = 'special'
-api_key = ''
-response = requests.get(f'https://www.dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={api_key}')
-response = requests.get('https://api.dictionaryapi.dev/api/v2/entries/en/'+word).json()
-# print(response)
-print(response)
-
+for word in wordsFile:
+    response = requests.get(f'https://www.dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={api_key}')
+    # time.sleep(1)
+    definition = str(response.json()[0].get('shortdef'))
+    # trim the [' and ']
+    definition = definition[:-2]
+    definition = definition[2:]
+    definition = definition.replace('\'','')
+    # write to a file, separate by a /t
+    write_me = word + '\t' + definition + '\n'
+    # write_me = '\''+word+'\''+':'+'\''+definition+'\','
+    words_definition.write(write_me)
 
 
 # value = 'aboriginal	Having existed in a region from the beginning'
