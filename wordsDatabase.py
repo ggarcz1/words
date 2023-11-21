cbbad48667b999a782f7e715321d6f0b04169741
ggarcz1 <54447564+ggarcz1@users.noreply.github.com>
2023-11-21 16:19:59 -0600

    updated the try except statement

--- wordsDatabase.py
+++ wordsDatabase.py
@@ -6,7 +6,7 @@ wordsFile = open('words.txt','r')
 words_definition = open('output.txt', 'a')
 api_key = '186c73a1-3a44-4091-9e6d-a2cdf0d47608'
 error = '\t Error: word was not found, check spelling\n'
-torf = True
+write_me = ''
 # test for network connectivity here
 
 for word in wordsFile:
@@ -14,16 +14,11 @@ for word in wordsFile:
     arr = word.split('\t') 
     if len(arr) == 2:
         break
-        
+
     response = requests.get(f'https://www.dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={api_key}')
 
     try:
         definition = str(response.json()[0].get('shortdef'))
-    except:
-        torf = False
-        write_me = word[:-1] + '' + error
-
-    if torf:
         # trim the [' and ']
         definition = definition[:-2]
         definition = definition[2:]
@@ -31,8 +26,12 @@ for word in wordsFile:
         # write to a file, separate by a /t
         # remove the \n from the word
         write_me = word[:-1] + '\t' + definition + '\n' 
-   
+    except:
+        write_me = word[:-1] + '' + error
+
     words_definition.write(write_me)
     write_me = ''
 
 print('Process completed.')
+
+
