85b44d504cd92b55e46dc0d8bcf296d745a2f269
ggarcz1 <54447564+ggarcz1@users.noreply.github.com>
2023-11-20 21:31:54 -0600

    error checks
    added error checks and filled up the database

--- wordsDatabase.py
+++ wordsDatabase.py
@@ -1,27 +1,36 @@
 import requests
 import time
-import json
 
 # open the file of words
 wordsFile = open('words.txt','r')
-words_definition = open('output.txt', 'w')
+words_definition = open('output.txt', 'w+')
 api_key = '186c73a1-3a44-4091-9e6d-a2cdf0d47608'
+error = '\t Error: word was not found, check spelling'
+torf = True
 
 for word in wordsFile:
+    # definition already exists
+    arr = word.split('\t') 
+    if len(arr) == 2:
+        break
+
     response = requests.get(f'https://www.dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={api_key}')
-    # time.sleep(1)
-    definition = str(response.json()[0].get('shortdef'))
-    # trim the [' and ']
-    definition = definition[:-2]
-    definition = definition[2:]
-    definition = definition.replace('\'','')
-    # write to a file, separate by a /t
-    write_me = word[:-1] + '\t' + definition + '\n'
-    # write_me = '\''+word+'\''+':'+'\''+definition+'\','
+
+    try:
+        definition = str(response.json()[0].get('shortdef'))
+    except:
+        torf = False
+        write_me = word + '' + error
+
+    if torf:
+        # trim the [' and ']
+        definition = definition[:-2]
+        definition = definition[2:]
+        definition = definition.replace('\'','')
+        # write to a file, separate by a /t
+        # remove the \n from the word
+        write_me = word[:-1] + '\t' + definition + '\n' 
+   
     words_definition.write(write_me)
 
-
-# value = 'aboriginal	Having existed in a region from the beginning'
-# print(value.split('\t'))
-
-
+print('Process completed.')
