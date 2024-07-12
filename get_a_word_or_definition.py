b5b0d5aab88a85ef6513ce3a84bfa334bdec9926
ggarcz1 <54447564+ggarcz1@users.noreply.github.com>
2024-07-12 10:51:16 -0500

    update to add phrases

--- get_a_word_or_definition.py
+++ get_a_word_or_definition.py
@@ -8,10 +8,12 @@ def search_database(search_string):
     if search_string == '':
         cursor.execute('SELECT * FROM words')
     else:
-        query = 'SELECT * FROM words WHERE definition LIKE ?'
+        query = 'SELECT * FROM words WHERE word LIKE ?'
         cursor.execute(query, ('%' + search_string + '%',))
-   
-    # check if user exists in the database already
+    # else:
+    #     query = 'SELECT * FROM words WHERE definition LIKE ?'
+    #     cursor.execute(query, ('%' + search_string + '%',))
+    # # check if user exists in the database already
     
     response = cursor.fetchall()
     sqliteConnection.close()
@@ -20,9 +22,9 @@ def search_database(search_string):
 
 if len(sys.argv) == 1:
     values = search_database(input('Enter data: '))
+    # def_or_word = search_database(input('Enter 1 for word and 2 for definiton search: '))
 else:
     values = search_database(sys.argv[1])
-
 
 for each in values:
     print(each)
--- wordsDatabase.py
+++ wordsDatabase.py
@@ -44,14 +44,14 @@ def check_for_word(word):
     sqliteConnection.close()
     return len(response) == 1
 
-
+wordsFileName = 'words.txt'
 # open the file to add new words
-wordsFile = open('words.txt','r')
+wordsFile = open(f'{wordsFileName}','r')
 # words_definition = open('words.txt', 'a')
 api_key = '186c73a1-3a44-4091-9e6d-a2cdf0d47608'
 error = '\t Error: word was not found, check spelling\n'
 write_me = ''
-counter = 0
+counter = counter_phrases = 0
 host_to_test = "google.com"
 port_to_test = 443
 duplicates = []
@@ -60,53 +60,61 @@ duplicates = []
 if not test_network_connectivity(host_to_test, port_to_test):
     exit(0)
 
+print(f'Searching for the words in the file {wordsFileName}...')
+
+write_me = definition = None
 
 for word in wordsFile:
     # check for if the definition already exists
     arr = word.split('\t')
-    if len(arr) == 1 and not check_for_word(arr[0]):
-        response = requests.get(f'https://www.dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={api_key}')
+    if check_for_word(arr[0]):
+        duplicates.append(arr[0])
+    else:
+        arr = word.split(' ')
+        if len(arr) == 1:
+            response = requests.get(f'https://www.dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={api_key}')
+        # phrase was added
+            try:
+                    definition = str(response.json()[0].get('shortdef'))
+                    # trim the [' and ']
+                    definition = definition[:-2]
+                    definition = definition[2:]
+                    definition = definition.replace('\'','')
+                    # write to a file, separate by a /t
+                    # remove the \n from the word
+                    counter += 1
+            except:
+                definition = error
 
-        try:
-            definition = str(response.json()[0].get('shortdef'))
-            # trim the [' and ']
-            definition = definition[:-2]
-            definition = definition[2:]
-            definition = definition.replace('\'','')
-            # write to a file, separate by a /t
-            # remove the \n from the word
-            write_me = word[:-1] + '\t' + definition + '\n' 
-            counter += 1
-        except:
-            write_me = word[:-1] + '' + error
+            values = [word, definition]
+            # sqliteConnection = sqlite3.connect('words.db')
+            # cursor = sqliteConnection.cursor()  
+            # cursor.execute("INSERT INTO words VALUES (?, ?)",values)
+            # sqliteConnection.commit()
 
-        values = [word, write_me]
+        # phrase present 
+        elif len(arr) > 1:
+            counter_phrases += 1
+            phrase = word
+            values = [word, None]
+            # write data to file
+
         sqliteConnection = sqlite3.connect('words.db')
         cursor = sqliteConnection.cursor()  
         cursor.execute("INSERT INTO words VALUES (?, ?)",values)
         sqliteConnection.commit()
-        # alphabetize
-        cursor.execute("SELECT word,definition from words ORDER BY word ASC")
-        sqliteConnection.commit()
-        sqliteConnection.close()
 
-        # words_definition.write(write_me)
-        write_me = ''
-    else:
-        duplicates.append(word)
-
-
-
-# if counter != 0:
-#     # write database to the output file 
-#     sqliteConnection = sqlite3.connect('words.db')
-#     cursor = sqliteConnection.cursor()  
-#     cursor.execute('SELECT * FROM words')
-#     response = cursor.fetchall()
-#     for each in response:
-#         print(each)
-#     sqliteConnection.close()
- 
+    sqliteConnection = sqlite3.connect('words.db')
+    cursor = sqliteConnection.cursor()  
+    cursor.execute('SELECT * FROM words')
+    response = cursor.fetchall()
+    for each in response:
+        print(each)
+        
+# alphabetize
+# cursor.execute("SELECT word,definition from words ORDER BY word ASC")
+# sqliteConnection.commit()
+# sqliteConnection.close()
 
 # overwrite the initial input file 
 wordsFile = open('words.txt','w')
@@ -114,6 +122,8 @@ wordsFile.write('')
 wordsFile.close()
 
 print('Process completed. ' + str(counter) + ' words have been searched.')
+print('Process completed. ' + str(counter_phrases) + ' phrases have been added.')
+
 if len(duplicates) != 0:
     print('The following duplicates were discovered: ')
     print(duplicates)
